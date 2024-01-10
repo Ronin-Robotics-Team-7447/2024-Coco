73d28313b9fd83f130086ff0383580cf76620296
Team Laptop <first.robotics.portola@gmail.com>
2024-01-10 15:49:21 -0800

    test github

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -9,7 +9,9 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ExampleSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
+  public ExampleSubsystem() {
+    // test robot code
+  }
 
   /**
    * Example command factory method.
